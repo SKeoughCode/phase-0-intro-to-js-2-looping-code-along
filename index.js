c0b65efb65b1602b5bb0486059c763fe818194a6
SKeoughCode <131558651+SKeoughCode@users.noreply.github.com>
2023-04-23 21:34:01 -0400

    Looping Final Commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    var thankYous = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYous;
+}
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+    return
+}
